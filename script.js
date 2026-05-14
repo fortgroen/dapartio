9c7f177408f86580de0f9026d8a2e1acf3bf8cd4
fortgroen <contact.fortgroen@gmail.com>
2026-05-14 13:27:41 +0200

    Add script.js to display current year

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
- (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
-diff --git a/script.js b/script.js
-new file mode 100644
-index 0000000000000000000000000000000000000000..46dc4849a4b8537915038deb2e5ea9b46a1be359
---- /dev/null
-+++ b/script.js
-@@ -0,0 +1 @@
-+document.getElementById('year').textContent = new Date().getFullYear();
- 
-EOF
-)
+document.getElementById('year').textContent = new Date().getFullYear();
